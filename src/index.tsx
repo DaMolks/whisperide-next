c3d97cb0384e65a68451adb935dc7114276e2575
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 23:25:42 +0100

    Fix splash screen entry point and update main entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from './theme/ThemeProvider';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import App from './App';
+import { darkTheme } from './theme';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
--- src/splash/index.tsx
+++ src/splash/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import Splash from './Splash';
 import { darkTheme } from '../theme';
-import { Splash } from './Splash';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Splash />
     </ThemeProvider>
   </React.StrictMode>
