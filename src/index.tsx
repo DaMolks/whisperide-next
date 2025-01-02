a497c589435ace157c3a5ea147faf32d3977d762
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 23:44:33 +0100

    Add and update entry points

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
 import { darkTheme } from './theme';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
