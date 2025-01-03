71d9a03f71a50caf42792107234984e441943457
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-03 17:23:35 +0100

    Update preload script

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,20 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
-console.log('Preload script starting...');
-
 contextBridge.exposeInMainWorld('electron', {
-  minimize: () => {
-    console.log('Sending minimize command...');
-    ipcRenderer.send('window-control', 'minimize');
-  },
-  maximize: () => {
-    console.log('Sending maximize command...');
-    ipcRenderer.send('window-control', 'maximize');
-  },
-  close: () => {
-    console.log('Sending close command...');
-    ipcRenderer.send('window-control', 'close');
-  }
+  minimize: () => ipcRenderer.send('window-control', 'minimize'),
+  maximize: () => ipcRenderer.send('window-control', 'maximize'),
+  close: () => ipcRenderer.send('window-control', 'close')
 });
-
-console.log('Preload script finished...');
