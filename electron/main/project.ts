6312f3d8bd8b108b58fc7706e54c2056a7cd86d6
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-04 16:08:56 +0100

    Fix import path in project.ts

--- electron/main/project.ts
+++ electron/main/project.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import type { ProjectInfo, ProjectSettings } from '../../shared/types';
+import type { ProjectInfo, ProjectSettings } from '../types';
 
 const execAsync = promisify(exec);
 
