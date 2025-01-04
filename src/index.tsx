f6ec64fadc2ebcd4afa2b774ab27ffda9d7efe62
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-04 21:41:29 +0100

    Add entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { darkTheme } from './theme';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(
+const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-).render(
+);
+
+root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
